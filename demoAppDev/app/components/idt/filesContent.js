351312c0dd1ded470747b568625d7c2c9e8cfb85
Vasily Kushakov <vasilykushakov@BCSR-10603.local>
2018-11-13 14:53:50 -0800

    Added xray scan to files tab

--- demoAppDev/app/components/idt/filesContent.js
+++ demoAppDev/app/components/idt/filesContent.js
@@ -2,8 +2,9 @@ import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import SVG from 'react-inlinesvg';
 import PhoneImagesIcon from '../../../icon/phone-images.svg';
-import FollowIcon from "../../../icon/follow-icon.svg";
 import BackArrow from '../../../icon/back-arrow.svg';
+import CloseHeader from '../../../icon/close-header.svg';
+import ShareIOS from '../../../icon/share-ios.svg';
 
 export default class filesContent extends Component{
     constructor(){
@@ -11,7 +12,9 @@ export default class filesContent extends Component{
         let date = new Date();
         this.toggleClass = this.toggleClass.bind(this);
         this.backButton = this.backButton.bind(this);
+        this.redirectFile = this.redirectFile.bind(this);
         this.state = {
+            redirect: true,
             chat: false,
             files: true,
             more: false,
@@ -47,25 +50,25 @@ export default class filesContent extends Component{
                     avatar: "/images/Word App.png",
                 },
                 {
-                    name: "DSC07052.jpg",
+                    name: "SalyerDarrellScan.png",
                     time: date.toLocaleString('en-US', { month: 'numeric', day: 'numeric' }),
                     message: "Modified by Alvin Tao on ",
                     svg: "PhoneImagesIcon",
                 },
                 {
-                    name: "DSC07053.jpg",
+                    name: "FluAwarenessPoster.png",
                     time: date.toLocaleString('en-US', { month: 'numeric', day: 'numeric' }),
                     message: "Modified by Alvin Tao on ",
                     svg: "PhoneImagesIcon",
                 },
                 {
-                    name: "DSC07054.jpg",
+                    name: "WilkersJohn.png",
                     time: date.toLocaleString('en-US', { month: 'numeric', day: 'numeric' }),
                     message: "Modified by Alvin Tao on ",
                     svg: "PhoneImagesIcon",
                 },
                 {
-                    name: "DSC07055.jpg",
+                    name: "RobeyVernScan.png",
                     time: date.toLocaleString('en-US', { month: 'numeric', day: 'numeric' }),
                     message: "Modified by Alvin Tao on ",
                     svg: "PhoneImagesIcon",
@@ -97,6 +100,13 @@ export default class filesContent extends Component{
             document.getElementsByClassName("page")[0].classList.add("page-selected");
         },100)
     }
+    redirectFile() {
+        this.setState((prevState) => {
+            return {
+                redirect: !prevState.redirect
+            }
+        });
+    }
 
     render() {
         const idtFileItem = this.state.idtFileItems.map(((item, key)=>{
@@ -106,7 +116,7 @@ export default class filesContent extends Component{
                         <span className="idt-files-container">
                             {item.avatar ? <img src={item.avatar} alt="file picture"/> : <SVG src={PhoneImagesIcon}/>}
                         </span>
-                        <ul>
+                        <ul onClick={this.redirectFile}>
                             <li className={"message-info"}><span>{item.name}</span></li>
                             <li className="message-info"><span className="message">{item.message}{item.time}</span></li>
                         </ul>
@@ -115,30 +125,54 @@ export default class filesContent extends Component{
             );
         }));
 
-        return (
+        if (this.state.redirect) {
+            return (
+                <section className="idt-chat">
+                    <header>
+                        <h2 className="header-center header-center-purple">IDT</h2>
+                        <h1 className="header-center header-center-under">Contoso Hospital</h1>
+                        <ul className="icon-container">
+                            <li className="back-arrow" onClick={this.backButton}><Link to={{pathname: '/teams', state: {prev: 'true'}}}><SVG
+                                src={BackArrow}/></Link></li>
+                        </ul>
+                        <ul className="idt-sub-nav">
+                            <li className={this.state.chat ? 'chat selected': "chat"} onClick={() => {this.toggleClass("chat")}}>
+                                <Link to={{pathname:'/idt', state:{next:'false'}}}>Chat</Link>
+                            </li>
+                            <li className={this.state.files ? 'files selected': "files"} onClick={() => {this.toggleClass("files")}}>
+                                <Link to={{pathname:'/files', state:{next:'false'}}}>Files</Link>
+                            </li>
+                            <li className={this.state.more ? 'more selected': "more"} onClick={() => {this.toggleClass("more")}}>
+                                <Link to={{pathname:'/more', state:{next:'false'}}}>More</Link>
+                            </li>
+                        </ul>
+                    </header>
+                    {idtFileItem}
+                </section>
+            )
+        } else{
+            return (
             <section className="idt-chat">
                 <header>
-                    <h2 className="header-center header-center-purple">IDT</h2>
-                    <h1 className="header-center header-center-under">Contoso Hospital</h1>
+                    <h2 className="header-center">SalyerDarrellScan.png</h2>
+                    <h1 className="header-center header-center-under">Contoso Hospital / IDT</h1>
                     <ul className="icon-container">
-                        <li className="back-arrow" onClick={this.backButton}><Link to={{pathname: '/teams', state: {prev: 'true'}}}><SVG
-                            src={BackArrow}/></Link></li>
-                        <li className="follow-icon"><SVG src={FollowIcon}/></li>
-                    </ul>
-                    <ul className="idt-sub-nav">
-                        <li className={this.state.chat ? 'chat selected': "chat"} onClick={() => {this.toggleClass("chat")}}>
-                            <Link to={{pathname:'/idt', state:{next:'false'}}}>Chat</Link>
-                        </li>
-                        <li className={this.state.files ? 'files selected': "files"} onClick={() => {this.toggleClass("files")}}>
-                            <Link to={{pathname:'/files', state:{next:'false'}}}>Files</Link>
-                        </li>
-                        <li className={this.state.more ? 'more selected': "more"} onClick={() => {this.toggleClass("more")}}>
-                            <Link to={{pathname:'/more', state:{next:'false'}}}>More</Link>
+                        <li className="back-close" onClick={this.redirectFile}><SVG
+                                src={CloseHeader}
+                                />
                         </li>
                     </ul>
                 </header>
-                {idtFileItem}
+                <div style={{background: "white", height: "100%"}}>
+                    <img style={{width: "100%", transform: "translateY(80%)"}}src={"/images/Scan.png"} alt="scan"></img>
+                </div>
+                <footer className="footer-2" id="single-chat-footer-2">
+                    <ul className="footer-icons">
+                        <li className="share-ios"><SVG src={ShareIOS}/></li>
+                    </ul>
+                </footer>
             </section>
-        )
+            )
+        }
     }
 }
